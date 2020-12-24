5c1a9e8282565d3181a80308d429370a4a5e32a2
Tzlil Shtam <tzlilshtam@gmail.com>
2020-12-24 12:32:15 +0200

    assets have been upload

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 15f;
+    [SerializeField]
+    float mapField = 5f;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+        newPosition.x = Mathf.Clamp(newPosition.x , -mapField , mapField);
+        rb.MovePosition(newPosition);
+    }
+}
